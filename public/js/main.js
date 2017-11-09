5b7611b4055be304b50fd907dc272f66e51b5d10
NuongNguyen1996 <nuong14pt@gmail.com>
2017-11-09 23:19:02 +0700

    Nuong push page read

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+ // // sroll to top, use animate function on jquery
+
+ //    (function(){
+ //         $('#cuon').click(function(){
+ //         $('body,html').animate({scrollTop:0},600);
+ //         return false;
+ //         })
+ //         })(jQuery);
+
+ //         $(window).scroll(function(){
+ //         if( $(window).scrollTop() == 0 ) {
+ //         $('#cuon').stop(false,true).fadeOut(300);
+ //         }else{
+ //         $('#cuon').stop(false,true).fadeIn(300);
+ //         }
+ //        });
